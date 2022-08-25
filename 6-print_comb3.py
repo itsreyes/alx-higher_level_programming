0724def07f357375a005eefe95631ccea0738bc2
Kings Udechukwu <105047301+itsreyes@users.noreply.github.com>
2022-08-25 23:30:18 +0100

    Delete 6-print_comb3.py

--- 6-print_comb3.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-for i in range(9):
-    for j in range(10):
-        if j > i:
-            if i != 8 or j != 9:
-                print("{:d}{:d}".format(i, j), end=", ")
-            else:
-                print("{:d}{:d}".format(i, j))
